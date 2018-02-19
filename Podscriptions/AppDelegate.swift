82dc718452dd1909ade4c99a6471ffd4d0383b06
Jeff Chimney <jeff.chimney@avssystems.ca>
2018-02-18 23:31:12 -0800

    Initial Commit

---
+++ Podscriptions/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Podscriptions
+//
+//  Created by Jeff Chimney on 2018-02-18.
+//  Copyright © 2018 Jeff Chimney. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Podscriptions/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Podscriptions
+//
+//  Created by Jeff Chimney on 2018-02-18.
+//  Copyright © 2018 Jeff Chimney. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
